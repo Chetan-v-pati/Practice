f4ebe619453ecf3255c91ba2f66f3bb0d6b64d89
Chetanpatil <cpatil211@gmail.com>
2022-01-03 16:33:43 +0530

    practice

---
+++ 7.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("Chetan Patil");
+
+var firstname = "Basit";
+console.log(firstname);
+firstname = "Bhim";
+console.log(firstname);
+
+var second = 10;
+console.log(second * 10);
